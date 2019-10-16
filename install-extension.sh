2fe602eea6fc7c7556af7dd1efaf91d62fe2c244
ampacheco <ampacheco@users.noreply.github.com>
2019-10-16 15:34:23 -0300

    Update install-extension.sh

--- install-extension.sh
+++ install-extension.sh
@@ -1,1 +1,2 @@
 az extension add -y --source https://azureclitemp.blob.core.windows.net/spring-cloud/spring_cloud-0.1.0-py2.py3-none-any.whl
+alias clean="az group delete -n $(az group list -o table | grep -i rg | awk '{print $1}') --no-wait --yes"
